55861016cc0c842006a66be3541ecce88a4b408c
Eva Ward <evaward@Evas-MacBook-Pro.local>
2016-12-25 17:41:55 -0500

    Update answer and code for Q5. datetime

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,29 @@
 # Hint:  use Google to find python function
+from datetime import datetime as dt
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+#function to convert to datetime.date and calculate difference
+def date_diff(date_start, date_stop, date_format):
+    dt_start = dt.strptime(date_start, date_format)
+    dt_stop = dt.strptime(date_stop, date_format)
+    diff = dt_stop - dt_start
+    return diff.days
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+####a)
+date_format = '%m-%d-%Y'
+date_start = '01-02-2013'
+date_stop = '07-28-2015'
+print 'a.', date_diff(date_start, date_stop, date_format)
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+####b)
+date_format = '%m%d%Y'
+date_start = '12312013'
+date_stop = '05282015'
+date_diff(date_start, date_stop, date_format)
+print 'b.', date_diff(date_start, date_stop, date_format)
+
+####c)
+date_format = '%d-%b-%Y'
+date_start = '15-Jan-1994'
+date_stop = '14-Jul-2015'
+date_diff(date_start, date_stop, date_format)
+print 'c.', date_diff(date_start, date_stop, date_format)
