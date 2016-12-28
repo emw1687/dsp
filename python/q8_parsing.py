b311f02e2a360d8079a18d40dc457e9a3126334b
Eva Ward <evaward@Evas-MacBook-Pro.local>
2016-12-28 00:17:39 -0500

    Update code for Q8. parsing

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,6 +1,20 @@
-# The football.csv file contains the results from the English Premier League. 
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of 
-# goals scored for and against each team in that season (so Arsenal scored 79 goals 
-# against opponents, and had 36 goals scored against them). Write a program to read the file, 
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+# The football.csv file contains the results from the English Premier League.
+# The columns labeled 'Goals' and 'Goals Allowed' contain the total number of
+# goals scored for and against each team in that season (so Arsenal scored 79 goals
+# against opponents, and had 36 goals scored against them). Write a program to read the file,
+# then print the name of the team with the smallest difference in 'for' and 'against' goals.
 
+handle = open("football.csv")
+diffs = {}
+
+for line in handle:
+    line = line.split(',')
+    try:
+        team = line[0]
+        diff = abs(int(line[5])-int(line[6]))
+        diffs[diff] = team
+    except:
+        continue
+
+desc = sorted(diffs)
+print diffs[desc[0]]
