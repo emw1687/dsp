5a23d1123d922cab66b167e77fede0677f8b010f
Eva Ward <evaward@Evas-MacBook-Pro.local>
2016-12-28 20:51:27 -0500

    Update code for Part II and add emails.csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv') as facultycsv:
+    emails = list() #all email addresses
+
+    for line in facultycsv:
+        line = line.split(',')
+        if line[0] == 'name' : continue
+        try:
+            email = line[3].rstrip()
+            emails.append(email)
+        except:
+            continue
+
+with open('emails.csv', 'w') as emailcsv:
+    writer = csv.writer(emailcsv, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for email in emails:
+        writer.writerow([email])
